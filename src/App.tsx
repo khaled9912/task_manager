c7b54cdce24875fcb012bb045bd372db3a54ea2b
khaled9912 <khaledmourad23345@gmail.com>
2024-08-22 15:20:24 +0300

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-   <main className="">task management app</main>
-        
-  )
+  return <main className="">task management app</main>;
 }
 
-export default App
+export default App;
